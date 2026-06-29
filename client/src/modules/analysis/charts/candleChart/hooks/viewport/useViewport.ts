d8d0579628cb254bf744df5ff5a1df7a32da9aae
ngth1010v <thinh0947709008@gmail.com>
2026-06-29 22:46:55 +0700

    [Fix][client][candleChart] fix auto price

--- client/src/modules/analysis/charts/candleChart/hooks/viewport/useViewport.ts
+++ client/src/modules/analysis/charts/candleChart/hooks/viewport/useViewport.ts
@@ -247,8 +247,8 @@ function useViewport(candleData: CandleData): Viewport {
     let minPrice = binOhlcs.l[0];
 
     for (let i = 1; i < binCount; i += 1) {
-      if (binOhlcs.h[i] > maxPrice) maxPrice = binOhlcs.h[i];
-      if (binOhlcs.l[i] < minPrice) minPrice = binOhlcs.l[i];
+      if (binOhlcs.h[i] != BigInt(0) && binOhlcs.h[i]> maxPrice) maxPrice = binOhlcs.h[i];
+      if (binOhlcs.l[i] != BigInt(0) && binOhlcs.l[i] < minPrice) minPrice = binOhlcs.l[i];
     }
 
     const priceRange = Number(maxPrice - minPrice);
