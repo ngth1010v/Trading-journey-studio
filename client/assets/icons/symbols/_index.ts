13ae49606081f1231c6b7b8bc487bd08479c5c7c
ngth1010v <thinh0947709008@gmail.com>
2026-06-29 16:25:55 +0700

    [Feat][client] add symbol icons

---
+++ client/assets/icons/symbols/_index.ts
@@ -0,0 +1,47 @@
+export const SymbolIcons = {
+    // Currency
+    AUD: "./Australia.png",
+    BRL: "./Brazil.png",
+    CAD: "./Canada.png",
+    CHF: "./Switzerland.png",
+    DKK: "./Denmark.png",
+    EUR: "./EU.png",
+    GBP: "./UK.png",
+    HKD: "./Hongkong.png",
+    JPY: "./Japan.png",
+    MXN: "./Mexico.png",
+    NOK: "./Norway.png",
+    NZD: "./NewZealand.png",
+    PLN: "./Poland.png",
+    USD: "./US.png",
+    ZAR: "./SouthAfrica.png",
+
+    // Commodity
+    CUC: "./Copper.png",
+    NGC: "./LiquefiedGas.png",
+    XAG: "./Silver.png",
+    XAU: "./Gold.png",
+    XPD: "./Palladium.png",
+    XPT: "./Platinum.png",
+    XTI: "./Oil.png",
+
+    // Cryptocurrency
+    BTC: "./Bitcoin.png",
+    DOG: "./Dogecoin.png",
+    ETH: "./Ethereum.png",
+    LTC: "./Litecoin.png",
+    SOL: "./Solana.png",
+    XRP: "./Ripplecoin.png",
+
+    // Index
+    DAX40   : "./Germany.png",
+    ESX     : "./EU.png",
+    F40     : "./France.png",
+    HSI     : "./Hongkong.png",
+    IBX     : "./Spain.png",
+    JPN255  : "./Japan.png",
+    NAS100  : "./US.png",
+    SP500   : "./US.png",
+    UK100   : "./UK.png",
+    US30    : "./US.png",
+} as const;
