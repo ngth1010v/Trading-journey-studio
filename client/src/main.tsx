2a7ba7f2dbdd9a41d397209250e4a30dd544bf14
ngth1010v <thinh0947709008@gmail.com>
2026-07-14 19:28:59 +0700

    [Feat][client] add default font

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ThemeDataProvider } from './modules/theme/ThemeDataProvider.tsx'
 import App from './App.tsx'
+import "./index.css";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
